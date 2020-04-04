b8ab08ab9b0235610c9aaabf11c79ab1e263ff45
holbertontaekim1107 <1701@holbertonschool.com>
2020-04-04 02:05:24 +0000

    strtok added to repo for multi inputs in command line

---
+++ strtok.c
@@ -0,0 +1,47 @@
+#include "shell.h"
+
+/**
+ *
+ *
+ *
+ */
+
+char **_strtok(char *line, char *delm)
+{
+	char *tok, **ptr;
+	int i;
+	size_t words = 0;
+
+	for (i = 0; line[i]; i++)
+	{
+		if (line[i] != *delm && (line[i + 1] == *delm || line[i + 1] == '\0'))
+				words++;
+	}
+	line[i - 1] = '\0';
+
+	ptr = malloc(sizeof(char *) * (words + 1));
+
+	if (!ptr)
+	{
+		printf("Error\n");
+		return (0);
+	}
+
+	tok = strtok(line, delm);
+
+	for (i = 0; tok != NULL; i++)
+	{
+		ptr[i] = malloc(strlen(tok) + 1);
+		if (!ptr[i])
+		{
+			for (i -= 1; i >= 0; i--)
+				free(ptr[i]);
+			exit;
+		}
+		strcpy(ptr[i], tok);
+		tok = strtok(NULL, delm);
+	}
+	ptr[i] = NULL;
+
+	return (tok);
+}
