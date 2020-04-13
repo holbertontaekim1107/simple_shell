205df0528eaf2cd3d29679b0528fff1b8ede70de
MenacingManatee <brett.p.davis101@gmail.com>
2020-04-13 22:30:21 +0000

    Dynamically finds path instead of it being hardcoded

--- _str.c
+++ _str.c
@@ -37,10 +37,36 @@ int _strlen(char *s)
  */
 int _strcmp(char *s1, char *s2)
 {
+	if (s1 == NULL || s2 == NULL)
+		return (-1);
+	if (*s1 == '\0' || *s2 == '\0')
+		return (*s1 - *s2);
 	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
 		s1++;
 		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 - *s2);
+}
+/**
+ * _strncmp - compares n bytes of two strings
+ * @s1: string 1 to be compared
+ * @s2: string 2 to be compared
+ * @n: max bytes
+ * Return: a positive, negative, or 0 number based on the first different char
+ */
+int _strncmp(char *s1, char *s2, int n)
+{
+	int i = 0;
+
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2 && i < n)
+	{
+		s1++;
+		s2++;
+		i++;
 	}
 	if (*s1 == *s2)
 		return (0);
